53dea01e92856ce64bf882df2488cc081903feaa
Syed Affan Bukhari <affanshaxx091@gmail.com>
2025-12-31 16:19:11 +0500

    Add files via upload

---
+++ Car Price Prediction  (1).py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+
+# In[2]:
+
+
+df = pd.read_csv("CAR DATA1.csv")
+df
+
+
+# In[3]:
+
+
+df.isnull().sum()
+
+
+# In[4]:
+
+
+df.duplicated()
+
+
+# In[5]:
+
+
+df["Car_Age"] = 2025 - df["Year"] 
+
+
+# In[6]:
+
+
+df.drop(["Year", "Car_Name"], axis=1, inplace=True) 
+
+
+# In[7]:
+
+
+df
+
+
+# In[8]:
+
+
+df = pd.get_dummies(
+    df,
+    columns=['Fuel_Type', 'Selling_type', 'Transmission'],
+    drop_first=True
+)
+
+
+# In[9]:
+
+
+df
+
+
+# In[10]:
+
+
+X = df.drop('Selling_Price', axis=1)
+y = df['Selling_Price']
+
+
+# In[11]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+# In[12]:
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
+
+# In[13]:
+
+
+lr = LinearRegression()
+rf = RandomForestRegressor()
+
+
+# In[14]:
+
+
+lr.fit(X_train, y_train)
+rf.fit(X_train, y_train)
+
+
+# In[15]:
+
+
+y_lr = lr.predict(X_test)
+y_rf = rf.predict(X_test)
+
+
+# In[16]:
+
+
+
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error,accuracy_score,confusion_matrix
+print("/////////// Evaluation Report ///////////")
+print("------ LINEAR REGRESSION ------")
+
+print(f"Mean Absolute Error (MAE): {mean_absolute_error(y_test, y_lr):.3f}")
+print(f"Mean Squared Error (MSE): {mean_squared_error(y_test, y_lr):.3f}")
+print(f"Root Mean Squared Error (RMSE): {(mean_squared_error(y_test, y_lr))**0.5:.3f}")
+print(f"R2 Score: {r2_score(y_test, y_lr):.3f}")
+
+
+# In[17]:
+
+
+print("\n------ RANDOM FOREST REGRESSOR ------")
+print(f"Mean Absolute Error (MAE): {mean_absolute_error(y_test, y_rf):.3f}")
+print(f"Mean Squared Error (MSE): {mean_squared_error(y_test, y_rf):.3f}")
+print(f"Root Mean Squared Error (RMSE): {(mean_squared_error(y_test, y_rf))**0.5:.3f}")
+print(f"R2 Score: {r2_score(y_test, y_rf):.3f}")
+
+
+# In[18]:
+
+
+
+df['Predicted_Price_LR'] = lr.predict(X)
+
+
+df['Good_Deal_LR'] = (df['Selling_Price'] < df['Predicted_Price_LR']).astype(int)
+
+
+# In[19]:
+
+
+
+df['Predicted_Price_RF'] = rf.predict(X)
+
+
+df['Good_Deal_RF'] = (df['Selling_Price'] < df['Predicted_Price_RF']).astype(int)
+
+
+# In[20]:
+
+
+print("Good deals (Linear Regression):", df['Good_Deal_LR'].sum())
+print("Good deals (Random Forest):", df['Good_Deal_RF'].sum())
+
+
+# “Linear Regression shows perfect Good_Deal accuracy because its predicted prices are very close to actual prices, making the derived rule match exactly. Random Forest is more accurate overall in predicting prices (lower MAE, higher R²), but because its predictions fluctuate slightly around actual prices, the derived Good_Deal metric is less perfect. This illustrates that Good_Deal accuracy is a secondary metric and does not directly measure price prediction performance.”
+# Random Forest is more accurate and predicts selling prices more reliably. Linear Regression is simpler and interpretable but slightly less precise.
